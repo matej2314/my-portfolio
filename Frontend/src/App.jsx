ff9e0fea156f307745a07ef67432d1cea389e26a
matej2314 <mateo2314@gmail.com>
2024-11-07 11:51:57 +0100

    basic config of the project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return <div>
+    <p className="text-2xl text-red-600">Portfolio webpage</p>
+    <p className="text-xl text-orange-500">Welcome</p>
+  </div>
+}
+
+export default App
