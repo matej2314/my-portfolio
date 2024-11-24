f539f077384921f01355b0fe80445a79e261e584
matej2314 <mateo2314@gmail.com>
2024-11-24 12:06:12 +0100

    .

--- Backend/routes/dataRoutes.js
+++ Backend/routes/dataRoutes.js
@@ -7,7 +7,7 @@ router.use(express.json());
 
 router.get('/all', async (req, res) => {
   const queries = [
-    `SELECT 'posts' AS source, id, post_title AS title, post_content AS content, post_date, COALESCE(post_imageName, '') AS post_imageName FROM posts ORDER BY id;`,
+    `SELECT 'posts' AS source, id, post_title AS title, post_content AS contentg, post_date, COALESCE(post_imageName, '') AS post_imageName FROM posts ORDER BY id;`,
     `SELECT 'projects' AS source, pr.id, pr.project_name AS title, pr.project_category AS category, pr.project_URL AS link, pr.project_screenName, pr.project_description AS description, pr.repo AS repo, pr.long_text AS long_text FROM projects pr;`,
     `SELECT 'services' AS source, s.id, s.serviceName AS title, s.serviceDescription AS description FROM services s;`,
     `SELECT 'skills' AS source, s.id, s.skill_name AS title, s.skill_cat AS category, s.icon_name AS icon, s.icon_color as iconColor FROM skills s;`,
