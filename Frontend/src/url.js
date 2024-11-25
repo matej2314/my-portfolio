2c4289b7711fbc35bf5193951853c2dce85a5fed
matej2314 <mateo2314@gmail.com>
2024-11-25 19:56:53 +0100

    .

--- Frontend/src/url.js
+++ Frontend/src/url.js
@@ -1,5 +1,5 @@
 const urlFromLocal = 'http://185.170.196.107:5051';
-const urlFromServer = 'http://127.0.0.1:5051';
+const urlFromServer = 'http://172.18.0.6:5051';
 
 export const backendUrl = `${urlFromServer}/data/collection`;
 export const imgUrl = `${urlFromServer}/images`;
