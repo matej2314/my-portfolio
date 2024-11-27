d50b667f67fb6022a165f77ae40f6b79228b2cc1
matej2314 <mateo2314@gmail.com>
2024-11-27 22:13:51 +0100

    Modified social icons styles

--- Frontend/src/components/icons/iconsClasses.js
+++ Frontend/src/components/icons/iconsClasses.js
@@ -1,7 +1,7 @@
 
 export const iconsClasses = {
     socialIcons: {
-        iconsWrapper: "w-fit h-fit flex justify-around gap-3 indirect:mt-2",
+        iconsWrapper: "w-fit h-fit flex justify-around gap-3 mt-2 indirect:mt-2",
         facebookColor: "#1877F2",
         githubColor: "gray-500/15",
         linkedinColor: "#0A66C2",
