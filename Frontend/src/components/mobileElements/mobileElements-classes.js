3b13dac41c7e2fd8bfc96d8b86e0c0ed6b0845a2
matej2314 <mateo2314@gmail.com>
2024-11-27 20:10:10 +0100

    .

--- Frontend/src/components/mobileElements/mobileElements-classes.js
+++ Frontend/src/components/mobileElements/mobileElements-classes.js
@@ -3,6 +3,6 @@
          wrapper: "relative w-content flex items-center flex-row-reverse mb-2",
          button: "p-2 bg-neutral-700 hover:bg-neutral-600 focus:text-lime-500 text-[1rem] text-slate-300 ml-2 rounded-md",
          ul: "w-content h-fit flex flex-row justify-between items-center bg-neutral-700 text-slate-200 text-base p-2 rounded-md",
-         li: "text-[1rem] sm:text-base focus:text-lime-500"
+         li: "text-[0.7rem] sm:text-base focus:text-lime-500"
 }
 }
