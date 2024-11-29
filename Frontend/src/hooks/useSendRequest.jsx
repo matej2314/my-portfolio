d7600ebe011eb6ffe054f9ab1bb39340aef0ae67
matej2314 <mateo2314@gmail.com>
2024-11-29 22:58:10 +0100

    .

--- Frontend/src/hooks/useSendRequest.jsx
+++ Frontend/src/hooks/useSendRequest.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function useSendRequest() {
+export function useSendRequest() {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
     const [result, setResult] = useState(null);
@@ -35,5 +35,3 @@ function useSendRequest() {
 
     return { sendRequest, result, isLoading, error };
 }
-
-export default useSendRequest;
