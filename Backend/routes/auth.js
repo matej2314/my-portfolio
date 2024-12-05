90460d52ee028356ed886bdd2fd9186b3408d0e3
matej2314 <mateo2314@gmail.com>
2024-12-05 15:04:43 +0100

    .

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -5,10 +5,12 @@ const verifyJWT = require('../controllers/verifyJWT');
 
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
-// router.get('/verify', verifyJWT(), (req, res) => {
-//     res.status(200).json({
-//         userId: req.userId,
-//         userName: req.userName,
-//         role: req.role,
-//     });
-// });
+router.get('/verify', verifyJWT(), (req, res) => {
+    res.status(200).json({
+        userId: req.userId,
+        userName: req.userName,
+        role: req.role,
+    });
+});
+
+module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -22,13 +22,13 @@ app.use(cors({
     credentials: true,
 }));
 
-// app.options('*', (req, res) => {
-//     res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization',);
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     res.status(200).end();
-// });
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type',);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.status(200).end();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
