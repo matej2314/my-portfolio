a2f537102b49f0c5b7e648f87d42548d66686d34
matej2314 <mateo2314@gmail.com>
2024-12-07 11:06:06 +0100

    Displayed about me descriptions in management sec.

--- Frontend/src/components/cms/cms-components/ManageAbout.jsx
+++ Frontend/src/components/cms/cms-components/ManageAbout.jsx
@@ -5,10 +5,24 @@ import { cmsComponents } from "./cms-componenst-styles"
 
 
 export default function ManageAbout() {
-    const { refreshData } = useContext(DataContext);
+    const { fetchedData, refreshData, isLoading } = useContext(DataContext);
+    const about = fetchedData.data.about;
 
 
     return (
-        <p>Manage about me description</p>
+        <div>
+            <ul>
+                {!isLoading && about && Array.isArray(about) ? (
+                    about.map((text) => {
+                        <li>
+                            <span>{text.id}</span>
+                            <span>{text.aboutText}</span>
+                        </li>
+                    })
+                ) : (
+                    <p>Brak opisów.</p>
+                )}
+            </ul>
+        </div>
     )
 }
