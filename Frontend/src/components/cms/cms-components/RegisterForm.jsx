656f3185ab6e3bd9c86a9550c443f0358d8f4e9a
matej2314 <mateo2314@gmail.com>
2024-12-13 14:00:24 +0100

    .

--- Frontend/src/components/cms/cms-components/RegisterForm.jsx
+++ Frontend/src/components/cms/cms-components/RegisterForm.jsx
@@ -27,7 +27,7 @@ export default function RegisterForm() {
 
         const response = await register(username.current.value, email.current.value, password.current.value);
 
-        if (response.status === 200) {
+        if (response.status == 200) {
             toast.info('Użytkownik zarejestrowany. Możesz się zalogować.');
             setTimeout(() => {
                 navigate('login_admin');
