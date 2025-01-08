a1847174fb08516affaedcf2b83d7f863eb0262d
matej2314 <mateo2314@gmail.com>
2025-01-08 11:38:40 +0100

    Changes in server config

--- Backend/database/authQueries.js
+++ Backend/database/authQueries.js
@@ -2,4 +2,4 @@ module.exports = {
     registerAdminCheck: 'SELECT * FROM users WHERE role = "admin" LIMIT 1',
     register: 'INSERT INTO users SET ?',
     login: 'SELECT * FROM users WHERE email = ?',
-}
+};
