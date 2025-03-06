46924a6d421fa3205d3bc06dc4a3d17c2b3546ee
matej2314 <mateo2314@gmail.com>
2025-03-06 12:10:32 +0100

    Another config of CORS

--- Backend/server.js
+++ Backend/server.js
@@ -25,13 +25,13 @@ app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-// app.options('*', (req, res) => {
-//     res.header('Access-Control-Allow-Origin', req.get('Origin'));
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization',);
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     res.status(200).end();
-// });
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', req.get('Origin'));
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization',);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.status(200).end();
+});
 
 app.use(express.static(path.join(__dirname, 'CMS')));
 app.use('/images', express.static(path.join(__dirname, 'projects-photos')));
